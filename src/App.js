8ef04d094ab29daa257620315aef1014bbf7a688
Ben Kwak <65254308+bkwak@users.noreply.github.com>
2021-01-26 15:49:45 -0800

    original test

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import _ from 'lodash';
 import axios from 'axios';
 import './App.css';
-import Table from './Table.js'
-import Search from './Search.js'
+
 
 const API_URL = 'https://data.townofcary.org/api/v2/catalog/datasets/parks-and-recreation-feature-map/exports/json';
 const DISPLAY_FIELDS = [
@@ -23,23 +22,10 @@ const DISPLAY_FIELDS = [
 
 class App extends Component {
 
-  constructor() {
-    super();
-    this.state = {
-      cat: 'name'
-    }
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(value) {
-    this.setState({cat: value})
-  }
-
   render() {
     return (
       <div className="App">
-        <Search change={this.handleChange} fields={DISPLAY_FIELDS} selection={this.state.cat}/>
-        <Table url={API_URL} fields={DISPLAY_FIELDS} selection={this.state.cat}/>
+        <h1>Hello World</h1> 
       </div>
     );
   }
